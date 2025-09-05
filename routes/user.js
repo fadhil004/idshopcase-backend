347ac7064950a0ea3fa99aa65c070bd070540552
Farraz05 <farrazaqil950@gmail.com>
2025-09-05 20:25:00 +0700

    Create Admin API for Manage User (CRUD)

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const { authenticate } = require("../middlewares/auth");
-const upload = require("../middlewares/upload");
+const { authenticate, authorizeAdmin } = require("../middlewares/auth");
+const { uploadProfile } = require("../middlewares/upload");
 
 // Profile
 router.get("/profile", authenticate, userController.getProfile);
-router.put("/profile", authenticate, upload.single("profile_picture"), userController.updateProfile);
+router.put(
+  "/profile",
+  authenticate,
+  uploadProfile.single("profile_picture"),
+  userController.updateProfile
+);
 router.put("/profile/password", authenticate, userController.updatePassword);
 
 // Addresses
 router.post("/addresses", authenticate, userController.addAddress);
 router.put("/addresses/:id", authenticate, userController.updateAddress);
 router.delete("/addresses/:id", authenticate, userController.deleteAddress);
+
+// Admin Panel
+router.get("/", authenticate, authorizeAdmin, userController.getAllUsers);
+router.get("/:id", authenticate, authorizeAdmin, userController.getUserById);
+router.put(
+  "/:id",
+  authenticate,
+  authorizeAdmin,
+  userController.updateUserByAdmin
+);
+router.delete("/:id", authenticate, authorizeAdmin, userController.deleteUser);
 
 module.exports = router;
---
+++ seeders/20250905124733-create-admin-user.js
@@ -0,0 +1,32 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const hashedPassword = await bcrypt.hashSync(process.env.PASS_ADMIN, 10); 
+
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name: "Admin",
+          email: "admin@mail.com",
+          password: hashedPassword,
+          phone: "08123456789",
+          profile_picture: null,
+          role: "admin",
+          resetPasswordToken: null,
+          resetPasswordExpire: null,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", { email: "admin@mail.com" }, {});
+  },
+};
